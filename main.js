d22029733b3826e1257b9467a85f86539a7a4a51
SUSSYJONNY <128854842+SUSSYJONNY@users.noreply.github.com>
2023-07-25 10:40:49 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,26 +1,78 @@
-menuListArray = ["Pizza Vegetariana", "Pizza Mozzarella", "Pizza  Pepperonni", "Pizza  Abacaxi (a mais vendida)"];
+canvas = document.getElementById('myCanvas;');
+ctx = canvas.getContext("2d");
 
+img_width = 300;
+img_height = 100;
 
-function getMenu() {
-    var htmldata = "";
-    menuListArray.sort();
-    for (var i = 0; i < menuListArray.length; i++) {
-        htmldata = htmldata + menuListArray[i] + '<br>';
-    }
-    document.getElementById("displayMenu").innerHTML = htmldata;
+var img_image;
+
+img_x = 100;
+img_y = 100;
+
+function add() {
+	img_imgTag = new Image();
+	img_imgTag.onload = uploadimg;
+	img_imgTag.src = img_image;
 }
 
-function addItem() {
-    var htmldata;
-    var item = document.getElementById("addItem").value;
-    var imgTags = '<img id="im1" src="images/pizzaImg.png"/>'
-    menuListArray.sort();
-    for (var i = 0; i < menuListArray.length; i++) {
-        htmldata = htmldata + imgTags + menuListArray[i] + '<br>';
-    }
-    document.getElementById("displayAddedMenu").innerHTML = htmldata;
+function uploadimg() {
+
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
 }
 
-function addTop() {
-    var item = document.getElementById("addItem").value; menuListArray.push(item); addItem();
+window.addEventListener("keydown", my_keydown)
+
+function my_keydown(e) {
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+
+	if ((keyPressed >= 97 && keyPressed <= 122) || (keyPressed >= 65 && keyPressed <= 90)) {
+		aplhabetkey(); 
+		document.getElementById("d1").innerHTML = "Você pressionou uma tecla do alfabeto"; 
+		console.log("aplhabet key");
+
+	} else if (keyPressed >= 48 && keyPressed <= 57) 
+	{
+		 numberkey(); document.getElementById("d1").innerHTML = "Você prssionou uma tecla número";
+		  console.log("Number key");
+		 } else if (keyPressed >= 37 && keyPressed <= 40)
+		  {
+			 arrowkey();
+			  document.getElementById("d1").innerHTML = "Você pressionou uma tecla direcional";
+			   console.log("Arrow Key");
+			 } 
+			 else if (keyPressed == 17 || keyPressed == 18 || keyPressed == 27)
+			  { specialkey();
+				 document.getElementById("d1").innerHTML = "Você pressionou ctrl/esc/alt";
+				  console.log("special key"); 
+				} else { otherkey(); 
+					document.getElementById("d1").innerHTML = "Você pressionou um simbolo ou outra tecla";
+					 console.log("other key");
+					 }
+} 
+		
+	
+
+
+
+function aplhabetkey() {
+	img_image = "alfabeto.png";
+	add();
 }
+function numberkey() {
+	img_image = "número.png";
+	add();
+}
+function arrowkey() {
+	img_image = "direcional.png";
+	add();
+}
+function specialkey() {
+	img_image = "especial.png";
+	add();
+}
+function otherkey() {
+	img_image = "outras.png";
+	add();
+}
+
